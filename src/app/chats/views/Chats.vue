c9132240b65bfb1465e9dd21777caed0d88d1717
Oseias Dev <oseiasbetodev@gmail.com>
2025-12-06 13:27:23 +0000

    Add font

--- src/app/chats/views/Chats.vue
+++ src/app/chats/views/Chats.vue
@@ -3,7 +3,7 @@
         class="shrink-0 fixed flex items-center top-0 w-full z-[100] px-5 h-[52px] border-b border-border-primary bg-background-primary">
         <div class="flex items-center justify-between">
             <div>
-                <h2 class=" text-2xl font-secondary font-bold text-text-primary">Mensagens</h2>
+                <h2 class=" text-[26px] font-secondary font-bold text-text-primary">Mensagens</h2>
             </div>
 
             <div>
--- src/app/users/components/SearchWrapper.vue
+++ src/app/users/components/SearchWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-text-secondary text-sm bg-background-secondary rounded-[30px] py-2.5 px-4">
+    <div class="text-text-secondary text-sm bg-background-secondary rounded-[30px] py-3 px-4">
         <p>Pesquisar</p>
     </div>
 </template>
