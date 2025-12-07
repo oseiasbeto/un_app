b8c57d47b7278c9abe4157314ded27a56c4ac99e
Oseias Dev <oseiasbetodev@gmail.com>
2025-12-07 20:21:11 +0000

    Tratando o esquema de recompensas

--- src/app/wallets/views/Rewards.vue
+++ src/app/wallets/views/Rewards.vue
@@ -1,21 +1,41 @@
 <template>
     <h1>Recompensas</h1>
 
-    <button @click="openRewardsAd">Obter Recompensas</button>
+    <button :disabled="loading" @click="openRewardsAd">Obter Recompensas</button>
 </template>
 
 
 <script setup>
 
+import { ref } from "vue";
 import {
     rewardsAd,
 } from "webtonative/AdMob";
 
+
+const loading = ref(false)
+
 const openRewardsAd = () => {
+    loading.value = true
     rewardsAd({
         adId: "ca-app-pub-3940256099942544/5224354917",
-        rewardsAdCallback: (value) => {
-            console.log(value)
+        rewardsAdCallback: (status) => {
+            // Trata o status do anúncio de recompensas
+            if(status === "success") {
+                loading.value = false
+            }
+
+            if(status === 'adDismissed') {
+                loading.value = false
+            }
+
+            if(status === 'adLoadFailure') {
+                loading.value = false   
+            }
+            if(status === 'rewardSuccess') {
+                alert("Recompensa concedida!")
+                loading.value = false
+            }
         }
     })
 }
